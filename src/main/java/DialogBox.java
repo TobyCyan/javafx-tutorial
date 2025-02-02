9f4467a709e21b6963ed1c448d3b924d8be00ae0
YongJunXi <ernestyongjunxi@gmail.com>
2025-02-02 15:19:12 +0800

    Add FXML and set up scene builder

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,5 +1,7 @@
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
@@ -7,30 +9,37 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
+import java.io.IOException;
+import java.util.Collections;
+
 public class DialogBox extends HBox {
-    private Label text;
+    @FXML
+    private Label dialog;
+    @FXML
     private ImageView displayIcon;
 
     public DialogBox(String text, Image icon) {
-        this.text = new Label(text);
-        this.displayIcon = new ImageView(icon);
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainWindow.class.getResource("/view/DialogBox.fxml"));
+            fxmlLoader.setController(this);
+            fxmlLoader.setRoot(this);
+            fxmlLoader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        this.text.setWrapText(true);
-        this.displayIcon.setFitWidth(100.0);
-        this.displayIcon.setFitHeight(100.0);
-        this.setAlignment(Pos.TOP_RIGHT);
-
-        this.getChildren().addAll(this.text, this.displayIcon);
+        dialog.setText(text);
+        displayIcon.setImage(icon);
     }
 
     /**
      * Flips the dialog box such that the ImageView is on the left and text on the right.
      */
     private void flip() {
-        this.setAlignment(Pos.TOP_LEFT);
         ObservableList<Node> tmp = FXCollections.observableArrayList(this.getChildren());
-        FXCollections.reverse(tmp);
-        this.getChildren().setAll(tmp);
+        Collections.reverse(tmp);
+        getChildren().setAll(tmp);
+        setAlignment(Pos.TOP_LEFT);
     }
 
     public static DialogBox getUserDialog(String text, Image icon) {
@@ -38,8 +47,8 @@ public class DialogBox extends HBox {
     }
 
     public static DialogBox getDukeDialog(String text, Image icon) {
-        DialogBox dialogBox = new DialogBox(text, icon);
-        dialogBox.flip();
-        return dialogBox;
+        DialogBox db = new DialogBox(text, icon);
+        db.flip();
+        return db;
     }
 }
--- src/main/java/MainWindow.java
+++ src/main/java/MainWindow.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
-public class MainWindow {
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+    @FXML
+    private VBox dialogContainer;
+
+    private Image userImg = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImg = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+    private Duke duke = new Duke();
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Duke instance */
+    public void setDuke(Duke d) {
+        duke = d;
+    }
+
+    /**
+     * Creates a dialog box containing user input, and appends it to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String userText = userInput.getText();
+        String dukeText = duke.getResponse(userText);
+
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(userText, userImg),
+                DialogBox.getDukeDialog(dukeText, dukeImg)
+        );
+        userInput.clear();
+    }
 }
